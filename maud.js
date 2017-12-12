29e6569f82afa088358cae0e3bf0ede096acc5ff
Mauud <maud.barollet@free.fr>
2017-12-12 17:07:20 +0100

    commit maud
    test colab

---
+++ maud.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mutl(a, b) {
+    return a * b;
+}
+
+function ui(a, b) {
+    return a - b + c;
+}
+
+function game(a, b) {
+    return a;
+}
